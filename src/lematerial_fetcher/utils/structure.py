8d77e2345935c2a99e358bcba48dc28e04e6aff9
amandinerossello <amandine.rossello@entalpic.ai>
2025-09-09 08:03:04 +0000

    feat: differenciate get optimade from pymatgen for oc20

--- src/lematerial_fetcher/utils/structure.py
+++ src/lematerial_fetcher/utils/structure.py
@@ -40,8 +40,79 @@ def get_composition_reduced_from_descriptive_formula(batch):
     return batch
 
 
-def get_optimade_from_pymatgen(
-    structure: Structure, role: str = None, name: str = None, immutable_id: str = None
+def get_optimade_from_pymatgen(structure: Structure) -> dict:
+    """
+    Extracts the possible fields from a pymatgen Structure object
+    that are compatible with the OPTIMADE schema.
+
+    Parameters
+    ----------
+    structure : Structure
+        A pymatgen Structure object
+
+    Returns
+    -------
+    dict
+        An OPTIMADE-compliant dictionary containing partial structure data
+        necessary to create an OptimadeStructure object.
+    """
+    # Basic chemistry fields
+    elements = sorted(list(set(str(site.specie) for site in structure.sites)))
+    # Note that this function returns a different result than the composition.to_reduced_dict method of pymatgen
+    reduced_dict = structure.composition.to_reduced_dict
+    elements_ratios = get_element_ratios_from_composition_reduced(reduced_dict)
+
+    # Formula fields
+    chemical_formula_reduced = get_composition_reduced_from_reduced_dict(reduced_dict)
+    chemical_formula_anonymous = structure.composition.anonymized_formula
+    # TODO(Ramlaoui): Maybe we should use the factor here?
+    chemical_formula_descriptive = structure.composition.formula
+
+    # Site and position data
+    cartesian_site_positions = structure.cart_coords.tolist()
+    species_at_sites = [str(site.specie) for site in structure.sites]
+    species = [
+        {
+            "mass": None,
+            "name": element,
+            "attached": None,
+            "nattached": None,
+            "concentration": [1],
+            "original_name": None,
+            "chemical_symbols": [element],
+        }
+        for element in elements
+    ]
+
+    # Structure metadata
+    nsites = len(structure.sites)
+    nelements = len(elements)
+
+    # Lattice and dimensionality
+    lattice_vectors = structure.lattice.matrix.tolist()
+    nperiodic_dimensions = 3  # Assuming 3D structure, adjust if needed
+    dimension_types = [1, 1, 1]  # Assuming 3D periodic, adjust if needed
+
+    return {
+        # Required fields
+        "elements": elements,
+        "nelements": nelements,
+        "elements_ratios": elements_ratios,
+        "nsites": nsites,
+        "cartesian_site_positions": cartesian_site_positions,
+        "species_at_sites": species_at_sites,
+        "species": species,
+        "chemical_formula_anonymous": chemical_formula_anonymous,
+        "chemical_formula_descriptive": chemical_formula_descriptive,
+        "chemical_formula_reduced": chemical_formula_reduced,
+        "dimension_types": dimension_types,
+        "nperiodic_dimensions": nperiodic_dimensions,
+        "lattice_vectors": lattice_vectors,
+    }
+
+
+def get_optimade_from_pymatgen_oc20(
+    structure: Structure, role: str = None, immutable_id: str = None
 ) -> dict:
     """
     Extracts the possible fields from a pymatgen Structure object
@@ -92,6 +163,7 @@ def get_optimade_from_pymatgen(
     if role == "molecule":
         dimension_types = [0, 0, 0]
         nperiodic_dimensions = 0
+        name = chemical_formula_reduced + "gas"
     elif role == "slab":
         # Identifying the largest lattice vector (i.e., the vacuum direction).
         lengths = [np.linalg.norm(vec) for vec in lattice_vectors]
@@ -99,9 +171,11 @@ def get_optimade_from_pymatgen(
         dimension_types = [1, 1, 1]
         dimension_types[non_periodic_axis] = 0
         nperiodic_dimensions = 2
+        name = "star"
     else:  # bulk, adslab, other
         dimension_types = [1, 1, 1]
         nperiodic_dimensions = 3
+        name = chemical_formula_reduced + "star"
 
     # Structure metadata
     nsites = len(structure.sites)
